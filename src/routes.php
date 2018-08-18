841e129f9e084ab676b9cb5d842637966145e81a
nwcra791 <labego_s@etna-alternance.net>
2018-08-18 16:21:15 +0200

    etape2 part 1

--- src/routes.php
+++ src/routes.php
@@ -15,13 +15,31 @@ $app->get('/api/recipes', function (Request $request, Response $response, array 
 	$data = mysqli_query($this->mysqli, $query);
 	$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
 	$responseArray = array(
-		'code' => '200',
+		'code' => 200,
 		'message' => 'OK',
 		'datas' => $data,
 	);
 	$json_data = json_encode($responseArray);
 	$response->getBody()->write($json_data);
 	// Render index view
+
+	return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/api/recipes/[{arg1}].json', function (Request $request, Response $response, array $args) {
+	$this->logger->info("Slim-Skeleton '/' route");
+
+	$query = "SELECT * FROM `recipes__recipe` WHERE slug = '".$args[arg1]."'";
+
+	$data = mysqli_query($this->mysqli, $query);
+	$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
+	$responseArray = array(
+		'code' => 200,
+		'message' => 'OK',
+		'datas' => $data,
+	);
+	$json_data = json_encode($responseArray);
+	$response->getBody()->write($json_data);
 
 	return $this->renderer->render($response, 'index.phtml', $args);
 });
@@ -35,7 +53,7 @@ $app->get('/api/recipes.json', function (Request $request, Response $response, a
 	$data = mysqli_query($this->mysqli, $query);
 	$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
 	$responseArray = array(
-		'code' => '200',
+		'code' => 200,
 		'message' => 'OK',
 		'datas' => $data,
 	);
@@ -51,9 +69,8 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
 	$this->logger->info("Slim-Skeleton '/' route");
 
 	$responseArray = array(
-		'code' => '404',
-		'message' => 'Not Found',
-		'datas' => array(),
+		'code' => 404,
+		'message' => 'Not Found'
 	);
 	$json_data = json_encode($responseArray);
 	$response->getBody()->write($json_data);
@@ -65,9 +82,8 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
 $app->get('/api/[{name}]', function (Request $request, Response $response, array $args) {
 	$this->logger->info("Slim-Skeleton '/' route");
 	$responseArray = array(
-		'code' => '404',
+		'code' => 404,
 		'message' => 'Not Found',
-		'datas' => array(),
 	);
 	$json_data = json_encode($responseArray);
 	$response->getBody()->write($json_data);
