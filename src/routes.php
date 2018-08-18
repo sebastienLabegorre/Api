d2c9f947a6da160a4fd7c6037428c85b921a2e2e
nwcra791 <labego_s@etna-alternance.net>
2018-08-18 21:26:50 +0200

    etape 2 part 2

--- src/routes.php
+++ src/routes.php
@@ -35,7 +35,7 @@ $app->get('/api/recipes/[{arg1}]', function (Request $request, Response $respons
 	$slugs = mysqli_fetch_all($slugs, MYSQLI_ASSOC);
 	foreach ($slugs as $key => $value) {
 		if ($value["slug"] == $recherche) {
-			$query = "SELECT * FROM `recipes__recipe` WHERE slug = '".$recherche."'";
+			$query = "SELECT id, name, slug, step FROM `recipes__recipe` WHERE slug = '".$recherche."'";
 			$data = mysqli_query($this->mysqli, $query);
 			$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
 			$responseArray = array(
@@ -53,6 +53,7 @@ $app->get('/api/recipes/[{arg1}]', function (Request $request, Response $respons
 		'message' => 'Not Found',
 	);
 	$json_data = json_encode($responseArray);
+	$response = $response->withStatus(404, 'Not Found');
 	$response->getBody()->write($json_data);
 	return $this->renderer->render($response, 'index.phtml', $args);
 });
@@ -86,6 +87,7 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
 		'message' => 'Not Found'
 	);
 	$json_data = json_encode($responseArray);
+	$response = $response->withStatus(404, 'Not Found');
 	$response->getBody()->write($json_data);
 	// Render index view
 
@@ -99,6 +101,7 @@ $app->get('/api/[{name}]', function (Request $request, Response $response, array
 		'message' => 'Not Found',
 	);
 	$json_data = json_encode($responseArray);
+	$response = $response->withStatus(404, 'Not Found');
 	$response->getBody()->write($json_data);
 	return $this->renderer->render($response, 'index.phtml', $args);
 });
