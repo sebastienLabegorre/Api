21e8cc3c2980c2ca2cb111982c413169982969d0
nwcra791 <labego_s@etna-alternance.net>
2018-08-18 13:53:47 +0200

    D - // A - recipes.json

--- src/routes.php
+++ src/routes.php
@@ -26,6 +26,26 @@ $app->get('/api/recipes', function (Request $request, Response $response, array 
 	return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+$app->get('/api/recipes.json', function (Request $request, Response $response, array $args) {
+	// Sample log message
+	$this->logger->info("Slim-Skeleton '/' route");
+
+	$query = "SELECT id, user_id, name, slug FROM `recipes__recipe`";
+
+	$data = mysqli_query($this->mysqli, $query);
+	$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
+	$responseArray = array(
+		'code' => '200',
+		'message' => 'OK',
+		'datas' => $data,
+	);
+	$json_data = json_encode($responseArray);
+	$response->getBody()->write($json_data);
+	// Render index view
+
+	return $this->renderer->render($response, 'index.phtml', $args);
+});
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
 	// Sample log message
 	$this->logger->info("Slim-Skeleton '/' route");
@@ -43,18 +63,6 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
 });
 
 $app->get('/api/[{name}]', function (Request $request, Response $response, array $args) {
-	$this->logger->info("Slim-Skeleton '/' route");
-	$responseArray = array(
-		'code' => '404',
-		'message' => 'Not Found',
-		'datas' => array(),
-	);
-	$json_data = json_encode($responseArray);
-	$response->getBody()->write($json_data);
-	return $this->renderer->render($response, 'index.phtml', $args);
-});
-
-$app->get('/[{arg1}]/[{arg2}]', function (Request $request, Response $response, array $args) {
 	$this->logger->info("Slim-Skeleton '/' route");
 	$responseArray = array(
 		'code' => '404',
