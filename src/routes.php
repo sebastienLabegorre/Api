1d903f6408759e81c0b511c4fb83ef342ff81090
nwcra791 <labego_s@etna-alternance.net>
2018-09-24 22:31:11 +0200

    add function recherche0

--- src/routes.php
+++ src/routes.php
@@ -505,6 +505,14 @@ $app->get('/api/recipes.json', function (Request $request, Response $response, a
 
 	$data = mysqli_query($this->mysqli, $query);
 	$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
+
+	if (isset($request->params('name'))) {
+		$recherche = $request->params('name');
+		foreach ($data as $key => $value) {
+			var_dump($value);
+		}
+	}
+	
 	$responseArray = array(
 		'code' => 200,
 		'message' => 'OK',
