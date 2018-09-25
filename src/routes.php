8cd4939f5ac536fb47dac866dffabb5845edc310
nwcra791 <labego_s@etna-alternance.net>
2018-09-25 21:39:26 +0200

    derniére étapes 2

--- src/routes.php
+++ src/routes.php
@@ -479,14 +479,14 @@ $app->get('/api/recipes/[{arg1}]', function (Request $request, Response $respons
 			$user = mysqli_query($this->mysqli, $query);
 			$user = mysqli_fetch_all($user, MYSQLI_ASSOC);
 			$user = $user[0];
-			echo "<pre>";
-			var_dump($data);
-			if ($password != $data['password']) {
-				unset($data['email']);
+			/*echo "<pre>";
+			var_dump($user);*/
+			if ($password != $user['password']) {
+				unset($user['email']);
 			}
-			unset($data['password']);
-			var_dump($data);
-			echo "</pre>";
+			unset($user['password']);
+			/*var_dump($user);
+			echo "</pre>";*/
 			$responseArray = array(
 				'code' => 200,
 				'message' => 'OK',
